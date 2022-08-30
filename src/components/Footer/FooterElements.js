f79fe252a4f386d8d1da5bd3b5d36b093cf441f9
Arian Ansari <esfahanarian@gmail.com>
2022-08-29 17:27:30 -0700

    minor works on the footer done. contact page to be implemented.

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -1,17 +1,14 @@
 import styled from "styled-components";
 
 export const FooterContainer = styled.div`
-    display: grid;
-    grid-template-columns: repeat(12, 1fr);
-    grid-column-gap: 10px;
+    display: flex;
     background-color: grey;
     
 `
 export const About = styled.div`
-    display: flex;
-    grid-column: 2/12;
-    margin-top: 1rem;
-    width: 50%;
+    display: grid;
+    grid-template-columns: 30% 70%;
+    margin-left: 8%;
 `
 export const Picture = styled.div`
     padding-right: 5%;
@@ -21,18 +18,36 @@ export const Description = styled.div`
 
 `
 export const Contacts = styled.div`
-    grid-column: 9;
+    margin-left: 10%;
+    margin-top: 3%;
+    display: flex;
+    flex-direction: column;
 
 `
 export const LinkedIn = styled.div`
+    margin: 10%;
+    align-self: center;
 
 `
 export const GitHub = styled.div`
+    margin: 10%;
+    align-self: center;
 
 `
 export const MyPic = styled.img`
     height: 250px;
     margin-top: 5%;
     margin-bottom: 5%;
+    border-radius: 50%;
 
+`
+
+export const ContactMe = styled.button`
+    margin: 10%;
+
+    background-color: grey;
+    color: #26a0da;
+    border: none;
+    font-weight: 900;
+    font: bold;
 `
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Me from "../images/me.jpg";
 import Git from "../../icons/git.png"
-import { FooterContainer, About, Picture, Description, Contacts, LinkedIn, GitHub, MyPic } from './FooterElements';
+import Linked from "../../icons/linkedIn.png"
+import { FooterContainer, About, Picture, Description, Contacts, LinkedIn, GitHub, MyPic, ContactMe } from './FooterElements';
 
 export const Footer = () => {
     return (
@@ -11,20 +12,22 @@ export const Footer = () => {
                     <MyPic src={Me} />
                 </Picture>
                 <Description>
-                    <p>Thank you for visiting my portfolio website</p>
-                    <p>I keep picking up new projects so be sure to check back my website in future</p>
-                    <p>feel free to connect with me though LinkedIn using the link on the right</p>
+                    <p>Thank you for visiting my portfolio website.</p>
+                    <p>I am constantly working on new projects so be sure to check back my website in future.</p>
+                    <p>Feel free to connect with me though LinkedIn using the link on the right</p>
                 </Description>
             </About>
             <Contacts>
                 <LinkedIn>
-
+                    <a href="https://www.linkedin.com/in/4arians/">
+                        <img src={Linked} width="64px"></img>
+                    </a>
                 </LinkedIn>
                 <GitHub>
                     <a href="https://github.com/arian233">
-                        <img src={Git}></img> Github
-                    </a>
+                        <img src={Git}></img> </a>
                 </GitHub>
+                <ContactMe>GET IN TOUCH</ContactMe>
             </Contacts>
         </FooterContainer>
     )
